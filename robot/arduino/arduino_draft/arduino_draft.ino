befe8c3458918ae208eba815386023ae57906b51
leesiwon <87349601+siwon0305@users.noreply.github.com>
2024-09-19 15:53:13 +0900

    modified packet

--- robot/arduino/arduino_draft/arduino_draft.ino
+++ robot/arduino/arduino_draft/arduino_draft.ino
@@ -48,20 +48,7 @@ void loop()
         digitalWrite(R_MOTOR_IN1, HIGH);
         digitalWrite(R_MOTOR_IN2, LOW);
         analogWrite(R_MOTOR_PWM, rightSpeed);  // 오른쪽 모터 설정
-
-        // 모터 속도를 라즈베리파이로 전송
-        sendMotorData(leftSpeed, rightSpeed);
       } 
     }
   }
-}
-
-void sendMotorData(int leftSpeed, int rightSpeed)
-{
-    // 패킷 형태로 전송: "L:왼쪽속도 R:오른쪽속도\n"
-    Serial.print("L:");
-    Serial.print(leftSpeed);
-    Serial.print(" R:");
-    Serial.print(rightSpeed);
-    Serial.print("\n");  // 패킷 끝을 알리기 위해 개행문자 사용
 }
