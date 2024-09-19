21119a31726cf52a4f3bd048fc96b1fc52da68d6
leesiwon <87349601+siwon0305@users.noreply.github.com>
2024-09-19 19:14:10 +0900

    motor value

--- robot/arduino/arduino_draft/arduino_draft.ino
+++ robot/arduino/arduino_draft/arduino_draft.ino
@@ -8,19 +8,19 @@
 
 #define MOTOR_STBY 9  // 모터 스탠바이 핀
 
-int leftSpeed = 0;  // 왼쪽 모터 속도 저장
+int leftSpeed = 0;   // 왼쪽 모터 속도 저장
 int rightSpeed = 0;  // 오른쪽 모터 속도 저장
 
 void sendMotorData(int leftSpeed, int rightSpeed)
 {
   Serial.print("L:");
   Serial.print(leftSpeed);
-  Serial.print("R:");
+  Serial.print(" R:");
   Serial.print(rightSpeed);
   Serial.print("\n");
 }
 
-void setup() 
+void setup()
 {
     pinMode(L_MOTOR_IN1, OUTPUT);
     pinMode(L_MOTOR_IN2, OUTPUT);
@@ -35,28 +35,28 @@ void setup()
     Serial.begin(9600);              // 시리얼 통신 시작
 }
 
-void loop() 
+void loop()
 {
-  if (Serial.available()) 
-  {
-    String packet = Serial.readStringUntil('\n');  // 개행 문자('\n')가 올 때까지 읽기
-    
-    if (packet.length() >= 3) // 최소 3바이트 이상이어야 유효한 패킷
+    if (Serial.available() >= 3) // 최소 3바이트가 도착할 때까지 대기
     {
-      leftSpeed = packet[0];  // 1바이트 왼쪽 속도 수신 (0~255)
-      rightSpeed = packet[1];  // 1바이트 오른쪽 속도 수신 (0~255)
+        byte startByte = Serial.read();
 
-       
-      // 명령 ID가 10인지 확인 후 모터 제어
-      digitalWrite(L_MOTOR_IN1, LOW);
-      digitalWrite(L_MOTOR_IN2, HIGH);
-      analogWrite(L_MOTOR_PWM, leftSpeed);  // 왼쪽 모터 설정
+        if (startByte == 10)
+        {
+            leftSpeed = Serial.read();   // 왼쪽 모터 속도
+            rightSpeed = Serial.read();  // 오른쪽 모터 속도
 
-      digitalWrite(R_MOTOR_IN1, HIGH);
-      digitalWrite(R_MOTOR_IN2, LOW);
-      analogWrite(R_MOTOR_PWM, rightSpeed);  // 오른쪽 모터 설정
+            // 왼쪽 모터 제어
+            digitalWrite(L_MOTOR_IN1, LOW);
+            digitalWrite(L_MOTOR_IN2, HIGH);
+            analogWrite(L_MOTOR_PWM, leftSpeed);
 
-      sendMotorData(leftSpeed, rightSpeed);      
+            // 오른쪽 모터 제어
+            digitalWrite(R_MOTOR_IN1, HIGH);
+            digitalWrite(R_MOTOR_IN2, LOW);
+            analogWrite(R_MOTOR_PWM, rightSpeed);
+
+            sendMotorData(leftSpeed, rightSpeed);
+        }
     }
-  }
 }
