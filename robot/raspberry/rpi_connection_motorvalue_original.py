a4a797742bead6324bbafd6c089a7d44a913127b
LeeJaeHoon0 <leejaehun9766@naver.com>
2024-09-20 11:56:25 +0900

    add before raspberry connection motorvalue file

---
+++ robot/raspberry/rpi_connection_motorvalue_original.py
@@ -0,0 +1,131 @@
+import cv2
+import socket
+import struct
+import os
+import threading
+import time
+import serial
+
+# status
+# before_status = 0
+
+# Central server details
+CENTRAL_SERVER_IP = "192.168.0.147"
+CENTRAL_SERVER_PORT = 3141
+
+# Pollination server details (if needed)
+# POLLINATION_SERVER_IP = "192.168.0.42"
+# POLLINATION_SERVER_PORT = 9003
+
+def connect_to_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
+    try:
+        sock.connect((ip, port))
+        print(f"Connected to server at {ip}:{port}")
+    except Exception as e:
+        print(f"Error connecting to server {ip}:{port} - {e}")
+        sock = None
+    return sock
+
+def send_frame(sock, cam):
+    while True:
+        try:
+            # Capture frame from /dev/video0 or /dev/video1
+            ret, frame = cam.read()
+            if ret and sock:
+                # Encode frame from /dev/video0 or /dev/video1
+                _, buffer = cv2.imencode('.jpg', frame)
+                frame_data = buffer.tobytes()
+                frame_size = len(frame_data)
+                header = b'SF'
+                sock.sendall(header + struct.pack(">L", frame_size) + frame_data + b'\n')
+                print(f"Sent frame of size {frame_size} to {sock.getpeername()}")
+        except Exception as e:
+            print(f"Error capturing or sending {cam} frame: {e}")
+        # Limit to 30 frames per second
+        # time.sleep(1/30)
+
+def send_status(sock_central):
+    while True:
+        try:
+            # Check for device availability
+            arduino_avail = os.path.exists('/dev/ttyArduino')
+            vid0_avail = os.path.exists('/dev/video0')
+            vid1_avail = os.path.exists('/dev/video1')
+            status = 1 if arduino_avail and vid0_avail and vid1_avail else 0
+        
+            status_packet = b'CS' + status.to_bytes(1, byteorder="big")
+            # status_packet = struct.pack('2sh', b'CS', status)
+            sock_central.sendall(status_packet + b'\n')
+            print(f"Sent status: CS {status}")
+        except Exception as e:
+            print(f"Error sending status: {e}")
+        time.sleep(5)  # Send status every 10 seconds
+
+def recieve_motor(sock_central, ser):
+    while True:
+        msg = b''
+        while len(msg) < 4:
+            msg += sock_central.recv(4)
+            start = int.from_bytes(msg[:1], byteorder="big")
+
+        if start == 10 and msg.endswith(b'\n'):
+            #direction = msg[1:2].decode('utf-8')
+            direction = start
+            left_value = int.from_bytes(msg[1:2], byteorder="big")
+            right_value = int.from_bytes(msg[2:3], byteorder="big")
+
+            print(f"direction : {direction}, motor value : {left_value}, {right_value}")
+
+            cmd = msg[1:]
+            ser.write(cmd)
+            print(f"{cmd}")
+
+            ar_msg = b''
+            if ser.in_waiting > 0:
+                ar_msg = ser.read(ser.in_waiting)
+
+            if ar_msg != b'':
+                print(f"{ar_msg}")
+
+def main():
+    # Initialize cameras
+    cam0 = cv2.VideoCapture(0)
+    cam1 = cv2.VideoCapture(1)
+
+    # Connect to central server
+    central_sock = connect_to_server(CENTRAL_SERVER_IP, CENTRAL_SERVER_PORT)
+    if not central_sock:
+        return # Exit if connection fails
+
+    # Connect to pollination server
+    # pollination_sock = connect_to_server(POLLINATION_SERVER_IP, POLLINATION_SERVER_PORT)
+    # if not pollination_sock:
+    #     return
+
+    # connect arduino
+    ser = serial.Serial('/dev/ttyArduino', 9600)
+    # Create threads
+    frame0_thread = threading.Thread(target = send_frame, args = (central_sock, cam0))
+    # frame1_thread = threading.Thread(target = send_frame, args = (pollination_sock, cam1))
+    status_thread = threading.Thread(target = send_status, args = (central_sock,))
+    motor_command_thread = threading.Thread(target= recieve_motor, args= (central_sock, ser))
+
+    # Start threads
+    frame0_thread.start()
+    # frame1_thread.start()
+    status_thread.start()
+    motor_command_thread.start()
+
+    try:
+        frame0_thread.join()
+        # frame1_thread.join()
+        status_thread.join()
+        motor_command_thread.join()
+    
+    except KeyboardInterrupt:
+        print("Interrupted by user")
+
+if __name__ == "__main__":
+    main()
